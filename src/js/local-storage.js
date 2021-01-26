df65a05df85fc17f8703fce4cc30cb964c63f761
MaximRight <maxright1984@gmail.com>
2021-01-26 15:36:04 +0200

    code refactoring

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -10,40 +10,64 @@ populateTheme();
 
 themeSwitchToggleRef.addEventListener('change', onSwitchToggle);
 
+// function onSwitchToggle() {
+//   let currentLocalTheme = localStorage.getItem('theme');
+//   if (currentLocalTheme === theme.LIGHT) {
+//     localStorage.setItem('theme', theme.DARK);
+//     const newLocalTheme = localStorage.getItem('theme');
+
+//     bodyRef.classList.remove(currentLocalTheme);
+//     bodyRef.classList.add(newLocalTheme);
+//   }
+//   if (currentLocalTheme === theme.DARK) {
+//     localStorage.setItem('theme', theme.LIGHT);
+//     const newLocalTheme = localStorage.getItem('theme');
+
+//     bodyRef.classList.remove(currentLocalTheme);
+//     bodyRef.classList.add(newLocalTheme);
+//   }
+// }
+
+// От гениальной Елены Фроловой!
+
 function onSwitchToggle() {
-  let currentLocalTheme = localStorage.getItem('theme');
-  let parsedLocalTheme = JSON.parse(currentLocalTheme);
-  if (currentLocalTheme === `"${theme.LIGHT}"`) {
-    localStorage.setItem('theme', JSON.stringify(theme.DARK));
-    const switchedLocalTheme = localStorage.getItem('theme');
-    const parsedSwitchedTheme = JSON.parse(switchedLocalTheme);
-    bodyRef.classList.remove(parsedLocalTheme);
-    bodyRef.classList.add(parsedSwitchedTheme);
-  }
-  if (currentLocalTheme === `"${theme.DARK}"`) {
-    localStorage.setItem('theme', JSON.stringify(theme.LIGHT));
-    const switchedLocalTheme = localStorage.getItem('theme');
-    const parsedSwitchedTheme = JSON.parse(switchedLocalTheme);
-    bodyRef.classList.remove(parsedLocalTheme);
-    bodyRef.classList.add(parsedSwitchedTheme);
+  if (themeSwitchToggleRef.checked) {
+    localStorage.setItem('theme', theme.DARK);
+    bodyRef.classList.remove(theme.LIGHT);
+    bodyRef.classList.add(theme.DARK);
+  } else {
+    localStorage.setItem('theme', theme.LIGHT);
+    bodyRef.classList.remove(theme.DARK);
+    bodyRef.classList.add(theme.LIGHT);
   }
 }
+
+// От Гениальной Елены Фроловой!
 
 function populateTheme() {
-  if (!localStorage.getItem('theme')) {
-    localStorage.setItem('theme', JSON.stringify(theme.LIGHT));
-  }
-
   const actualTheme = localStorage.getItem('theme');
-  // console.log(actualTheme);
-  if (actualTheme === `"${theme.DARK}"`) {
+  !actualTheme
+    ? bodyRef.classList.add(theme.LIGHT)
+    : bodyRef.classList.add(actualTheme);
+  if (actualTheme === theme.DARK) {
     themeSwitchToggleRef.checked = true;
   }
-
-  if (localStorage.getItem('theme') === `"${theme.DARK}"`) {
-    const switchedLocalTheme = localStorage.getItem('theme');
-    const parsedSwitchedTheme = JSON.parse(switchedLocalTheme);
-
-    bodyRef.classList.add(parsedSwitchedTheme);
-  }
 }
+
+// function populateTheme() {
+//   if (!localStorage.getItem('theme')) {
+//     localStorage.setItem('theme', theme.LIGHT);
+//   }
+
+//   const actualTheme = localStorage.getItem('theme');
+
+//   if (actualTheme === theme.DARK) {
+//     themeSwitchToggleRef.checked = true;
+//   }
+
+//   if (localStorage.getItem('theme') === theme.DARK) {
+//     const newLocalTheme = localStorage.getItem('theme');
+
+//     bodyRef.classList.add(newLocalTheme);
+//   }
+// }
